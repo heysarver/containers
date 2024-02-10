59f296529d8fba23f68818952c238f1fa02e88ea
astromodder <22250203+astromodder@users.noreply.github.com>
2024-02-10 15:02:39 -0500

    add build script

---
+++ bitnami-wordpress/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -t|--tag)
+    TAG_NAME="$2"
+    shift
+    shift
+    ;;
+    *)
+    shift
+    ;;
+  esac
+done
+
+docker buildx build --platform linux/arm64,linux/amd64 --push --build-arg="TAG_NAME=$TAG_NAME" -t sarvent/bitnami-wordpress:$TAG_NAME .
