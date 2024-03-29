d3e6974ec68b91f2aedcd4471a1bb5d60b9bc4f8
astromodder <22250203+astromodder@users.noreply.github.com>
2024-03-29 16:41:47 -0400

    added local support and added create record support.

--- dynamic-ip/script.sh
+++ dynamic-ip/script.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+# Check if .env file exists and source it
+if [ -f .env ]; then
+  source .env
+fi
+
 domain="${DOMAIN}"
 host="${HOST}"
+local="${LOCAL:-False}"
 
-public_ip=$(curl -s http://ifconfig.io)
+if [ "$local" = "True" ]; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux
+    public_ip=$(ip route get 1 | awk '{print $NF;exit}')
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    default_interface=$(route get default | grep interface: | awk '{print $2}')
+    public_ip=$(ifconfig $default_interface | grep 'inet ' | awk '$1=="inet" {print $2}')
+  fi
+else
+  public_ip=$(curl -s http://ifconfig.io)
+fi
 
 dns_record=$(curl -s \
   "https://api.digitalocean.com/v2/domains/$domain/records?name=$host.$domain" \
@@ -11,7 +28,16 @@ dns_record=$(curl -s \
 dns_record_id=$(echo $dns_record | jq -r '.domain_records[0].id')
 dns_record_data=$(echo $dns_record | jq -r '.domain_records[0].data')
 
-if [ "$public_ip" != "$dns_record_data" ]; then
+if [ "$dns_record_id" = "null" ]; then
+  echo "DNS record does not exist"
+  echo "Creating DNS record for $host.$domain with IP $public_ip"
+  response=$(curl -s -X POST \
+              -H "Content-Type: application/json" \
+              -H "Authorization: Bearer ${DIGITALOCEAN_TOKEN}" \
+              -d "{\"type\":\"A\",\"name\":\"$host\",\"data\":\"$public_ip\"}" \
+              "https://api.digitalocean.com/v2/domains/$domain/records")
+  echo $response
+elif [ "$public_ip" != "$dns_record_data" ]; then
   echo "IP address has changed"
   echo "Updating DNS record $dns_record_id from $dns_record_data to $public_ip"
   response=$(curl -s -X PATCH \
