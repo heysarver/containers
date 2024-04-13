e6c181227ed6aaf55c4b0480f6220921f94c1f5d
astromodder <22250203+astromodder@users.noreply.github.com>
2024-04-12 21:41:42 -0400

    add build tags for wp

--- bitnami-wordpress/build.sh
+++ bitnami-wordpress/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Function to join array elements by a character
+join_by() { local IFS="$1"; shift; echo "$*"; }
+
 while [[ $# -gt 0 ]]; do
   key="$1"
 
@@ -15,4 +18,20 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-docker buildx build --platform linux/arm64,linux/amd64 --push --build-arg="TAG_NAME=$TAG_NAME" -t heysarver/bitnami-wordpress:$TAG_NAME .
+# Split the version number into its components
+IFS='.' read -ra VERSION_PARTS <<< "$TAG_NAME"
+
+# Create additional tags
+TAGS=""
+for index in "${!VERSION_PARTS[@]}"; do
+  TAGS+="-t heysarver/bitnami-wordpress:$(join_by . "${VERSION_PARTS[@]:0:$index+1}") "
+done
+
+# Add the "latest" tag
+TAGS+="-t heysarver/bitnami-wordpress:latest"
+
+# Print the Docker command before executing it
+echo "docker buildx build --platform linux/arm64,linux/amd64 --push --build-arg=\"TAG_NAME=$TAG_NAME\" $TAGS ."
+
+# Build and push the Docker image with all tags
+eval "docker buildx build --platform linux/arm64,linux/amd64 --push --build-arg=\"TAG_NAME=$TAG_NAME\" $TAGS ."
