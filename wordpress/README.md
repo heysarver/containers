3743e227e4bcb212003c8bdac0760ba08db7b318
heysarver <22250203+heysarver@users.noreply.github.com>
2025-09-18 22:35:46 -0400

    wp

---
+++ wordpress/README.md
@@ -0,0 +1,12 @@
+# wordpress
+
+Extends the wordpress:x.x.x-apache  image to include additional php modules.
+
+Additional extensions:
+  - redis
+
+## Building
+
+```bash
+$ TAG_NAME=x.x.x ./build.sh
+```
---
+++ wordpress/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Function to join array elements by a character
+join_by() { local IFS="$1"; shift; echo "$*"; }
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    -t|--tag)
+    TAG_NAME="$2"
+    shift
+    shift
+    ;;
+    *)
+    shift
+    ;;
+  esac
+done
+
+# Split the version number into its components
+IFS='.' read -ra VERSION_PARTS <<< "$TAG_NAME"
+
+# Create additional tags
+TAGS=""
+for index in "${!VERSION_PARTS[@]}"; do
+  TAGS+="-t heysarver/wordpress:$(join_by . "${VERSION_PARTS[@]:0:$index+1}") "
+done
+
+# Add the "latest" tag
+TAGS+="-t heysarver/wordpress:latest"
+
+# Print the Docker command before executing it
+echo "docker buildx build --platform linux/arm64,linux/amd64 --push --build-arg=\"TAG_NAME=$TAG_NAME\" $TAGS ."
+
+# Build and push the Docker image with all tags
+eval "docker buildx build --platform linux/arm64,linux/amd64 --push --build-arg=\"TAG_NAME=$TAG_NAME\" $TAGS ."
